3811434c333d2ecd31de4c2b05652fdf43f72cb7
Abbie <77949813+Abbie-Bowen@users.noreply.github.com>
2021-08-26 14:27:15 -0700

    Adding Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react'
 import axios from 'axios';
-// import {
-//     BrowserRouter,
-//     Route,
-//     Switch
-// } from 'react-router-dom'
+import {
+    BrowserRouter,
+    Route,
+    Switch
+} from 'react-router-dom'
 
 //components
 import Gallery from './Gallery';
-import Header from './Header';
 import Nav from './Nav';
 import NotFound from './NotFound';
+import SearchForm from './SearchForm';
 
 //API 
 import apiKey from '../config';
@@ -24,28 +24,31 @@ class App extends Component {
         };
     }
 
-    componentDidMount () {
-        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cats&per_page=24&format=json&nojsoncallback=1`)
-            .then(response => {
-                this.setState({
-                    photos: response.data.photos.photo
-                });
-            }).catch(error => {
-                console.log('Error fetching and parsing data', error);
+    performSearch = (query) => {
+        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+        .then(response => {
+            this.setState({
+                photos: response.data.photos.photo
             });
-
+        }).catch(error => {
+            console.log('Error fetching and parsing data', error);
+        });
     }
 
-    render () {
-        console.log(this.state.photos)
-        return (
-            <div className="container">
-                <Header />
-                {/* <Nav /> */}
-                <Gallery photos={this.state.photos}/>
-                {/* <NotFound /> */}
-            </div>
-        );
-    }
+   render () {
+            return (
+                <div className="container">
+                    <div className="title">
+                        <h1>Photo Gallery</h1>
+                        <SearchForm onSearch = {this.performSearch.bind(this)} />
+                    </div>
+                    {/* <Nav /> */}
+                    <Switch>
+                        <Route path="/search" render= { () => <Gallery photos={this.state.photos}/>} />
+                        <Route path="/no_results" component={NotFound} />
+                    </Switch>
+                </div>
+            );
+        }
 }
 export default App;
